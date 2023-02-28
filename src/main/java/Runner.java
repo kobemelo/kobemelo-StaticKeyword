afcbe9be37a571d0b24faa9bd0a3fd6df9a64e75
kobemelo <jgalarz95@gmail.com>
2023-02-28 18:22:58 +0000

    Static Committed

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,7 @@
 
 public class Runner {
+
+    String global_var;
     /**
      * This challenge is about interacting with static variables.
      *
@@ -22,6 +24,9 @@ public class Runner {
      * @param in the String value you should change global_var to in.
      */
     public void changeClassVar(String in){
+
+  
+        Class.global_var = in;        
 
     }
 }
